5f9c2fb4bc9ae0856ad0bef29996d3c82489f744
art177k <artur.kuzminykh@gmail.com>
2024-05-30 21:07:52 +0100

    M24 Delivery with alteration (1)

--- web-based tool/manual.php
+++ web-based tool/manual.php
@@ -9,13 +9,20 @@ include_once 'includes/header.php'
 
     
    <h2>Before using the tool for IFC integrity check:</h2>
-   <ul style="align-items: start;">
-    <li> (1) </li>
-    <li>agwhw</li>
-    <li>agwhw</li>
+   <ul>
+      <br>
+    <li> (1) Locate your IFC file into the 'uploads' folder </li>
+    <br>
+    <li>(2) Locate the 'xkt' version of the uploaded IFC file into the 'models_xkt' folder</li>
+    <br>
+    <li>(3) Check 'prerequisites.txt' to ensure you have ifcopenshell and required libraries installed</li>
+    <br>
+    <li>(4) You can now perform validation by identifying the file name (without extension) and validation scenario</li>
+    <br>
+    <li>Please, check the D2.6 report for more information.</li>
    </ul>
 
-   <p> Welcome to the RecycleBIM IFC Validation Tool! Please read the manual before you start using the platform.</p>
+   <!-- <p> Welcome to the RecycleBIM IFC Validation Tool! Please read the manual before you start using the platform.</p> -->
 
 </section>
 </body>
